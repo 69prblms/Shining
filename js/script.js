78b741a433f37e3eb5346ee53a719b16abac842c
pastya <ghb.y14@gmail.com>
2021-09-23 23:35:45 +0300

    schedule+else

--- js/script.js
+++ js/script.js
@@ -13,12 +13,51 @@
     
     
 // });
-new Swiper ('.starring__swiper', {
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+
+var swiper = new Swiper(".starring__swiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-prev',
+        prevEl: '.swiper-button-next'
     },
 });
+
+// var swiper = new Swiper(".images__swiper", {
+//     slidesPerView: "auto",
+//     navigation: {
+//         nextEl: '.swiper-button-prev',
+//         prevEl: '.swiper-button-next'
+//     },
+//     centeredSlides: true,
+//     slidesPerGroup: 1,
+//     initialSlide: 1,
+//     spaceBetween: 0,
+//     loop: true,
+// });
+
+
+
+var swiper = new Swiper(".images__swiper", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 2,
+    centeredSlides: true,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var swiper2 = new Swiper(".images__swiper-2", {
+    loop: true,
+    spaceBetween: 10,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
+
  
 
 

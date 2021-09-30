be154726481471c9099c7ec7fdb6ee3a225b74cb
pastya <ghb.y14@gmail.com>
2021-09-30 19:53:29 +0300

    adaptive

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@
     
 // });
 
-var swiper = new Swiper(".starring__swiper", {
+var starringSwiper = new Swiper(".starring__swiper", {
     slidesPerView: "auto",
     spaceBetween: 30,
     navigation: {
@@ -23,33 +23,80 @@ var swiper = new Swiper(".starring__swiper", {
     },
 });
 
-var swiper = new Swiper(".images__swiper", {
-    loop: true,
-    spaceBetween: 0,
-    slidesPerView: 2,
-    centeredSlides: true,
-    watchSlidesProgress: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  var swiper2 = new Swiper(".images__swiper-2", {
-    loop: true,
-    spaceBetween: 10,
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+// var swiper = new Swiper(".images__swiper-2", {
+//     loop: true,
+//     spaceBetween: 0,
+//     slidesPerView: 2,
+//     centeredSlides: true,
+//     watchSlidesProgress: true,
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+//   var swiper2 = new Swiper(".images__swiper", {
+//     loop: true,
+//     spaceBetween: 10,
+//     thumbs: {
+//       swiper: swiper,
+//     },
+//   });
 
-  var swiper = new Swiper(".else__swiper", {
+
+var imagesSwiper = new Swiper(".images__swiper", {
+  loop: true,
+  spaceBetween: 15,
+  slidesPerView: "9",
+  freeMode: true,
+  watchSlidesProgress: true,
+  
+});
+var imagesSwiper2 = new Swiper(".images__swiper-2", {
+  loop: true,
+  spaceBetween: 0,
+  slidesPerView: 2,
+  centeredSlides: true,
+  watchSlidesProgress: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: imagesSwiper
+  }
+});
+
+
+
+  var elseSwiper = new Swiper(".else__swiper", {
     slidesPerView: "auto",
     spaceBetween: 30,
     navigation: {
         nextEl: '.swiper-button-prev',
         prevEl: '.swiper-button-next'
     },
+    // breakpoints: {
+    //   768: {
+    //     slidesPerView: 1,
+    //     spaceBetween: 20,
+    //   }
+    // }
 });
+
+const aboutlist = document.querySelector ('.about__list');
+const aboutBtnMore = document.querySelector ('.about__btn-more')
+
+aboutBtnMore.addEventListener('click', () => {
+  aboutlist.classList.toggle('height-auto__about')
+});
+
+const factslist = document.querySelector ('.facts__content');
+const factsBtnMore = document.querySelector ('.facts__btn-more')
+
+factsBtnMore.addEventListener('click', () => {
+  factslist.classList.toggle('height-auto__facts')
+});
+
 
  
 
